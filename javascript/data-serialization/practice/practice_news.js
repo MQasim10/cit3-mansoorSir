9a5addd11d9bf136e9e65e87ec40ec8e69074ef2
Qasim Haleem <qasim.icp.student@gmail.com>
2025-01-30 12:58:32 +0500

    Mansoor Sir task completed

--- javascript/data-serialization/practice/practice_news.js
+++ javascript/data-serialization/practice/practice_news.js
@@ -1,10 +1,10 @@
 const {articles} = require('../data/others/news.json')
 // Tasks
 
-// 1. Name all the sources with null ids.
-// 2. Name all the sources with null authors.
-// 3. Create a new list with any of the keys that have null values.
-// 4. Print the title, content and description of the latest article.
+// 1. Name all the sources with null ids. Done
+// 2. Name all the sources with null authors.Done
+// 3. Create a new list with any of the keys that have null values. Done
+// 4. Print the title, content and description of the latest article. Done
     // let latest = articles
     //                     .map(n => n.publishedAt)
     //                     .sort((a, b) => b - a)[0]
@@ -15,6 +15,6 @@ const {articles} = require('../data/others/news.json')
     //                 console.log(`Content: ${n.content}`)
     //                 console.log(`Description: ${n.description}`)
     //             });
-// 5. Print the title, content and description of the oldest article.
-// 6. Print the article with the longest title.
-// 7. Print all the articles from the same source.
+// 5. Print the title, content and description of the oldest article. Done
+// 6. Print the article with the longest title. Done
+// 7. Print all the articles from the same source. Done
